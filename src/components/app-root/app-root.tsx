ee5b3761370e1aa3c50b692a92f65403bd6af744
saiy2k <saiy2k@gmail.com>
2021-01-27 13:48:33 +0530

    - Moved scripts and stylesheet linking to index.html  - Moved all tags inside body to charity-home component  - Close all self-closing tags <img />, <br />  - Change URLs to reflect new URL: assets/chartiy/*  - Change style=‘’ to style={{'’}}

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,19 +1,19 @@
 import { Component, h } from '@stencil/core';
 
 @Component({
-  tag: 'app-root',
-  styleUrl: 'app-root.css',
+    tag: 'app-root',
+    styleUrl: 'app-root.css',
 })
 export class AppRoot {
-  render() {
-    return (
-      <ion-app>
-        <ion-router useHash={false}>
-          <ion-route url="/" component="app-home" />
-          <ion-route url="/profile/:name" component="app-profile" />
-        </ion-router>
-        <ion-nav />
-      </ion-app>
-    );
-  }
+    render() {
+        return (
+            <ion-app>
+                <ion-router useHash={false}>
+                    <ion-route url="/" component="charity-home" />
+                    <ion-route url="/profile/:name" component="app-profile" />
+                </ion-router>
+                <ion-nav />
+            </ion-app>
+        );
+    }
 }
--- stencil.config.ts
+++ stencil.config.ts
@@ -3,11 +3,12 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalScript: 'src/global/app.ts',
-  globalStyle: 'src/global/app.css',
-  taskQueue: 'async',
-  outputTargets: [{
-    type: 'www',
-    serviceWorker: null
-  }],
+    globalScript: 'src/global/app.ts',
+    globalStyle: 'src/global/app.css',
+    taskQueue: 'async',
+    outputTargets: [{
+        type: 'www',
+        serviceWorker: null,
+        baseUrl:'https://css.thegrassroots.app/'
+    }],
 };
