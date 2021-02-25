b2d6fac7a734f23075224c8f97ff3d062e1a1379
saiy2k <saiy2k@gmail.com>
2021-02-25 08:52:10 +0530

    Color - Themable 1 of 2

--- src/templates/verso/common/footer/footer.tsx
+++ src/templates/verso/common/footer/footer.tsx
@@ -10,7 +10,99 @@ export class VersoFooter {
 
     render() {
         return (
-            <footer class="site-footer"></footer>
+            <footer class="verso-footer">
+                <div class="section verso-py-9 verso-demo-bg-orange-pattern">
+                    <div class="section-content">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="verso-widget widget_text">
+                                        <h4 class="verso-widget-header">About Us</h4>
+                                        <p>Why do the nations rage, and the peoples plot a vain thing? The kings of the earth take a stand, and the rulers take counsel together, against Yahweh, and against his Anointed.</p>
+                                        <p>Why do the nations rage, and the peoples plot a vain thing.</p>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="verso-widget widget_recent_entries">
+                                        <h4 class="verso-widget-header">Recent Posts</h4>
+
+                                        <ul>
+                                            <li class="text-truncate">
+                                                <a href="post.html" class="float-left verso-mr-1 verso-widget-image" title="Post title">
+                                                    <img alt="post image" src="/assets/verso/images/charity-01-100x100-notinclude.jpg" />
+                                                </a>
+                                                <a href="post.html">I will instruct you & teach you in the way you should go.</a>
+                                                <small class="post-date d-block">September 25, 2017</small>
+                                            </li>
+
+                                            <li class="text-truncate">
+                                                <a href="post.html" class="float-left verso-mr-1 verso-widget-image" title="Post title">
+                                                    <img alt="post image" src="/assets/verso/images/charity-02-100x100-notinclude.jpg" />
+                                                </a>
+                                                <a href="post.html"> Make a joyful shout to God!</a>
+                                                <small class="post-date d-block">June 12, 2017</small>
+                                            </li>
+                                            <li class="text-truncate">
+                                                <a href="post.html" class="float-left verso-mr-1 verso-widget-image" title="Post title">
+                                                    <img alt="post image" src="/assets/verso/images/charity-03-100x100-notinclude.jpg" />
+                                                </a>
+                                                <a href="post.html">Stronger Leads & new visitors</a>
+                                                <small class="post-date d-block">May 05, 2017</small>
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="verso-widget verso-widget-no-padding">
+                                        <a href="#">
+                                            <img src="/assets/verso/images/charity-map-500x300-notinclude.png" alt="image" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="section verso-py-1">
+                    <div class="section-content">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-6 text-sm-left">
+                                    <div class="verso-widget widget_text">
+                                        <p>
+                                            Verso Template. © Oxygenna 2017
+                                        </p>
+                                    </div>
+
+                                </div>
+                                <div class="col-sm-6 text-sm-right">
+                                    <div class="verso-widget widget_social">
+                                        <div class="verso-icon-set">
+                                            <a class="verso-icon-set-item verso-transition" href="#">
+                                                <i class="fa fa-instagram"></i>
+                                            </a>
+                                            <a class="verso-icon-set-item verso-transition" href="#">
+                                                <i class="fa fa-linkedin"></i>
+                                            </a>
+                                            <a class="verso-icon-set-item verso-transition" href="#">
+                                                <i class="fa fa-facebook"></i>
+                                            </a>
+                                            <a class="verso-icon-set-item verso-transition" href="#">
+                                                <i class="fa fa-twitter"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </footer>
         );
     }
 
--- src/templates/verso/common/header/header.tsx
+++ src/templates/verso/common/header/header.tsx
@@ -10,7 +10,88 @@ export class VersoHeader {
 
     render() {
         return (
-        <header id="header" class="site-header header-style-1"></header>
+        <div class="verso-header">
+            { /** Navigation */ }
+            <div class="verso-nav verso-nav-hover verso-nav-sticky verso-nav-layout-logo-l-menu-r">
+                <div class="verso-nav-inner">
+                    <div class="verso-nav-container">
+
+                        { /** Logo */ }
+                        <div class="verso-nav-brand">
+                            <a href="index.html">
+                                <img src="/assets/verso/images/charity-classic-logo-130x100.png" alt="verso" /> Charity
+                            </a>
+                        </div>
+
+                        { /** Mobile menu toggle button */ }
+                        <div class="verso-nav-mobile">
+                            <a id="nav-toggle" href="#">
+                                <span></span>
+                            </a>
+                        </div>
+
+                        { /** Menu One */ }
+                        <nav class="verso-nav-menu">
+                            <ul class="verso-nav-list">
+                                <li>
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+                                <li>
+                                    <a href="causes-list.html">Causes</a>
+                                    <ul class="verso-nav-dropdown closed">
+                                        <li>
+                                            <a href="causes-list.html">Causes List</a>
+                                        </li>
+                                        <li>
+                                            <a href="single-cause.html">Single Cause</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="blog.html">News</a>
+                                    <ul class="verso-nav-dropdown closed">
+                                        <li>
+                                            <a href="blog.html">Blog</a>
+                                        </li>
+                                        <li>
+                                            <a href="single-post.html">Single Post</a>
+                                        </li>
+                                        <li>
+                                            <a href="author.html">Author</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                                <li>
+                                    <a class="verso-icon-set-item verso-transition" href="#">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="verso-icon-set-item verso-transition" href="#">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div class="verso-nav-widget">
+                                <div class="verso-widget">
+                                    <div class="verso-icon-set">
+                                        <a class="verso-icon-set-item verso-transition verso-search-widget-button-open" href="#">
+                                            <i class="fa fa-search"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
         );
     }
 
